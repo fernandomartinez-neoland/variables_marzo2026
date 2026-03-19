ddd5ca1e4f14678b96f9cdacd1754f8546bc73a6
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-03-19 17:00:16 +0100

    numeros, string y booleano

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,25 @@
-let nombre="mario";
-let numero=145656156156;
-let boolean=false;
-const array=[];
-const json={}
+let nombre = "mario";
+let numero = 5;
+let boolean = false;
+const array = [];
+const json = {}
 
-let nombrecito=document.getElementById("nombre")
-nombrecito.textContent=nombre
+let nombrecito = document.getElementById("nombre")
+// nombrecito.textContent = nombre
 
 console.log(nombre)
 
-const text=document.getElementById("palabra")
+const text = document.getElementById("palabra")
 
-text.addEventListener("input",()=>{
-    nombrecito.textContent=text.value+nombre
+text.addEventListener("input", () => {
+    nombrecito.textContent = numero + parseInt(text.value)
+})
+
+
+let bool = document.getElementById("bool");
+const booleano = document.getElementById("booleano");
+booleano.addEventListener("click", () => {
+    const tf = bool.innerText === "true";
+    console.log(bool.innerText)
+    bool.innerText = !tf
 })
